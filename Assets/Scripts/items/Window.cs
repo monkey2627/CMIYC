54ce14a920283ab1c4b6dd29e1fa2bfc3b25157b
rodrian320 <2501820622@qq.com>
2025-08-03 12:11:00 +0800

    窗户

---
+++ Assets/Scripts/items/Window.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : ItemBase
+{
+    public override void inter()
+    {
+        base.inter();
+        if (interactCnt >= 0)
+        {
+            TipPopManager.instance.ShowTip("Target group spotted in the eastern bushes—potential escape accomplices. If only they understand Meow-rse code... " +
+                                           "Just go tap that DOORBELL! Hmm... You'll pay them with your secretly stashed tuna treats afterward.");
+        }
+        else
+        {
+            TipPopManager.instance.ShowTip("Operation initiated. All paws proceed as planned. Meow~");
+        }
+        interactCnt++;
+    }
+}
